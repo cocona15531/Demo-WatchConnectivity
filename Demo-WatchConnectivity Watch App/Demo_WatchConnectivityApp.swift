eedc040739dac846db6d1ab814ed9fec8a8e626a
issei.ueda <ron.ron633662@gmail.com>
2023-10-13 21:03:49 +0900

    first commit

---
+++ Demo-WatchConnectivity Watch App/Demo_WatchConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_WatchConnectivityApp.swift
+//  Demo-WatchConnectivity Watch App
+//
+//  Created by Issei Ueda on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct Demo_WatchConnectivity_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo-WatchConnectivity/Demo_WatchConnectivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_WatchConnectivityApp.swift
+//  Demo-WatchConnectivity
+//
+//  Created by Issei Ueda on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct Demo_WatchConnectivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
